29281e1cda92336f662d7dbecb584600be88b6bd
marko <marko.laban@l33tsystems.com>
2020-09-04 12:10:07 +1200

    Initial commit

---
+++ realnet-cli/realnet.py
@@ -0,0 +1,4 @@
+from pynecone import Shell
+
+class Realnet(Shell):
+    pass
---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="realnet",
+    version="0.0.1",
+    description="Realnet command line interface",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/virtual-space/realnet-cli",
+    author="Marko Laban",
+    author_email="makro.laban@l33tsystems.com",
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "realnet=realnet.__main__:main",
+        ]
+    },
+)
