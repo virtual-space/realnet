019d0dc9a89cca840d9b655b7b4711a62d9f9c59
Nitharsan <nitharsan.puwanendran@l33tsystems.com>
2022-01-26 09:10:48 +0000

    bumped_version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="realnet",
-    version="0.0.14",
+    version="0.0.15",
     description="Realnet command line interface",
     long_description=README,
     long_description_content_type="text/markdown",
