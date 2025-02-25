fe6950811c160d321919a5ea96e81c9205650e09
Marko Laban <marko.laban@virtualspace.io>
2025-02-25 20:19:42 +1300

    fixed incorrectly refactored router

--- realnet/runner/http/runner.py
+++ realnet/runner/http/runner.py
@@ -1,30 +1,10 @@
-from flask import Flask, jsonify
+from ..runner import Runner
+from .app import create_app
 from realnet.core.config import Config
-from realnet.core.provider import ContextProvider
 
-app = Flask(__name__, 
-           template_folder='../templates',
-           static_folder='../static')
-
-@app.route('/healthz')
-def health_check():
-    return jsonify({'status': 'ok'})
-
-class HttpRunner:
-    def __init__(self):
-        self.context_provider = None
-        self.app = app
-
-    def run(self, context_provider: ContextProvider):
-        """Run the HTTP server"""
-        self.context_provider = context_provider
-        
-        # Configure Flask app
+class HttpRunner(Runner):
+    
+    def run(self, context_provider):
+        app = create_app(context_provider)
         cfg = Config()
-        
-        # Run the Flask app
-        app.run(
-            host=cfg.get_server_host(),
-            port=int(cfg.get_server_port()),
-            debug=True
-        )
+        app.run(cfg.get_server_host(), cfg.get_server_port(), False)
